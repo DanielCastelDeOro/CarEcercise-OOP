deb3656fcd27076204fecd8ec2d6effe04df20ef
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-01 07:37:56 -0600

    Add project files.

---
+++ CarEcercise/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarEcercise
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Modle { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ CarEcercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarEcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Truck = new Car();
+            Truck.Make = "Ford";
+            Truck.Modle = "F250";
+            Truck.Year = 2021;
+            Console.WriteLine($"My work truck is a {Truck.Make} \nThe modle is a {Truck.Modle} \nIt was built in {Truck.Year}");
+        }
+    }
+}
