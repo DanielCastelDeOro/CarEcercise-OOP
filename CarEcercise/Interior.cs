6ebc0c3ea1e54a01512b609d0b8b554d080604f5
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-01 08:01:37 -0600

    final commit

---
+++ CarEcercise/Interior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarEcercise
+{
+    class Interior
+    {
+        public string Material { get; set; }
+        public int FitsNumberOfPeople { get; set; }
+        public bool HasElectricSeats { get; set; }
+
+    }
+}
--- CarEcercise/Program.cs
+++ CarEcercise/Program.cs
@@ -11,6 +11,28 @@ namespace CarEcercise
             Truck.Modle = "F250";
             Truck.Year = 2021;
             Console.WriteLine($"My work truck is a {Truck.Make} \nThe modle is a {Truck.Modle} \nIt was built in {Truck.Year}");
+            Console.WriteLine("------------------------------------------------------");
+            
+            Interior Inside = new Interior();
+            Inside.Material = "Cloth";
+            Inside.FitsNumberOfPeople = 5;
+            Inside.HasElectricSeats = false;
+            Console.WriteLine($"The Material is {Inside.Material}\nI can fit {Inside.FitsNumberOfPeople} people\nDoes it have electric seats? {Inside.HasElectricSeats}");
+            Console.WriteLine("------------------------------------------------------");
+
+            Car WifesCar = new Car();
+            WifesCar.Make = "Subaru";
+            WifesCar.Modle = "Forester";
+            WifesCar.Year = 2012;
+
+            Interior WifesCarInside = new Interior();
+            WifesCarInside.FitsNumberOfPeople = 5;
+            WifesCarInside.Material = "Cloth";
+            WifesCarInside.HasElectricSeats = false;
+
+            Console.WriteLine($"My wifes car is a {WifesCar.Year}, {WifesCar.Make}, {WifesCar.Modle}\nIt fits {WifesCarInside.FitsNumberOfPeople}, the material is {WifesCarInside.Material} and does it have electric seats? {WifesCarInside.HasElectricSeats}");
+            
         }
+        
     }
 }
